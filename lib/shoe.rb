d291a1a20c973d367f375105b1b2c41b54cee70d
Nancy Jackson <nbjackson23@gmail.com>
2020-08-13 10:43:39 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,48 @@
 # Make your shoe class here!
+class shoe
+  def initialize (brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+  
+  def color=(color)
+    @color = color
+  end
+  
+  def color
+    @color
+  end
+  
+  def size=(num)
+    @size = num
+  end
+  
+  def size
+    @size
+  end
+  
+  def material = (material)
+    @material
+  end
+  
+  def material
+    @material
+  end
+  
+  def condition=(new)
+    @condition
+  end
+  
+  def condition
+    @condition
+  end
+  def condition_of_shoe
+    puts "The shoe has been repaired."
+  end
+    
+  end
+  
+  
