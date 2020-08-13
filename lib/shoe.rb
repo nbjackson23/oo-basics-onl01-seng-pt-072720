68d521ecd317112fd2a6a1278bd98730885c962c
Nancy Jackson <nbjackson23@gmail.com>
2020-08-13 10:45:25 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,48 +1,15 @@
 # Make your shoe class here!
-class shoe
-  def initialize (brand)
+class Shoe
+  def initialize(brand)
     @brand = brand
   end
-  
-  def brand
-    @brand
+
+  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+
+  def cobble()
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
   end
-  
-  def color=(color)
-    @color = color
-  end
-  
-  def color
-    @color
-  end
-  
-  def size=(num)
-    @size = num
-  end
-  
-  def size
-    @size
-  end
-  
-  def material = (material)
-    @material
-  end
-  
-  def material
-    @material
-  end
-  
-  def condition=(new)
-    @condition
-  end
-  
-  def condition
-    @condition
-  end
-  def condition_of_shoe
-    puts "The shoe has been repaired."
-  end
-    
-  end
-  
-  
+
+end
